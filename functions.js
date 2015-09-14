e326a573803981cea5c13b267dff1b83e1761dda
Nora Top <top.nora@gmail.com>
2015-09-14 20:54:08 +0000

    update compareStringsForSort to sort ascending and add the array of object{name,email} with sort exemples

--- functions.js
+++ functions.js
@@ -4,12 +4,47 @@
 This function should take two strings, and return a value that sort can use to determine which is the longest string. 
 Finally, create an array of strings and try to get it sorted using your new function.*/
 
-//function to use with sort in order to put the longest string at first element in array
+//function to use with sort in order to put the longest string at last in array
 function compareStringsForSort(a,b) {
-        if (a.length > b.length) return -1;
-        if (a.length < b.length) return 1;
+        if (a.length > b.length) return 1;
+        if (a.length < b.length) return -1;
         return 0;
 }
 
 //Exemple of use for the compareStringsForSort with sort
-['word1', 'longestWordToGoFirst','word2'].sort(compareStringsForSort);
+['word1', 'longestWordToGoLast','word2'].sort(compareStringsForSort);
+
+
+/*Create an array of objects (don’t need to use new here, just regular object literals). 
+These objects will contain a name and email property. 
+Then, run some code that will sort your array by the longest name. 
+Then, run some code that will sort your array by e-mail address in alphabetical order.*/
+
+var listOfEmail = [
+    {name: 'Nora',
+    email: 'top.nora@gmail.com'},
+    {name: 'Nathan',
+    email: 'Nathan@gmail.com'},
+    {name: 'Ziad',
+    email: 'Ziad@gmail.com'},
+    {name: 'Karl',
+    email: 'Karl@gmail.com'},
+    {name: 'François',
+    email: 'François@gmail.com'}
+    ];
+    
+// sort listOfEmail by the longest name 
+listOfEmail.sort(function(a,b){
+	if (a.name.length > b.name.length) return 1;
+    if (a.name.length < b.name.length) return -1;
+    return 0;
+});
+
+
+
+// sort listOfEmail by email alphabetically
+listOfEmail.sort(function(a,b){
+	if (a.email.toLowerCase() > b.email.toLowerCase()) return 1;
+    if (a.email.toLowerCase() < b.email.toLowerCase()) return -1;
+    return 0;
+});
